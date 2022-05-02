dc6b7286df59f93f2034da10feab7531fe83d5af
moto4321 <moto432195@gmail.com>
2022-05-02 15:50:18 +0900

    pr template test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FeedsModule } from './feeds/feeds.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UsersModule, FeedsModule],
+  imports: [UsersModule, FeedsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
