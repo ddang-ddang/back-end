271076bbb11ac555e5b5109951f8bab56fcdf58e
moto4321 <moto432195@gmail.com>
2022-05-02 15:10:24 +0900

    commit after fork

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { FeedsModule } from './feeds/feeds.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, FeedsModule],
   controllers: [AppController],
   providers: [AppService],
 })
