e7cbd4ca17ecd9974650f738fa81a3d79efda87e
diasm <diasm2@gmail.com>
2022-05-02 17:02:24 +0700

    [feat] likes controller 그리고  service추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FeedsModule } from './feeds/feeds.module';
+import { LikesController } from './likes/likes.controller';
+import { LikesService } from './likes/likes.service';
 
 @Module({
   imports: [UsersModule, FeedsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, LikesController],
+  providers: [AppService, LikesService],
 })
 export class AppModule {}
