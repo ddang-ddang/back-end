321010a5231dfe5291ddd094b3a8a9aa8d3f91f6
reload1bronze <nature9th@gmail.com>
2022-05-05 14:15:34 +0900

    [feat] 퀘스트 컨트롤러 정리 - 모든 퀘스트 조회 (위도/경도 기준) - 특정 퀘스트 조회 (퀘스트id 기준)

--- src/quests/quests.controller.ts
+++ src/quests/quests.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { QuestsService } from './quests.service';
 
 @Controller('quests')
-export class QuestsController {}
+export class QuestsController {
+  constructor(private readonly questService: QuestsService) {}
+
+  @Get()
+  getAll(@Query('lat') lat: number, @Query('lng') lng: number): string {
+    // TODO: (exception) 쿼리 파라미터(위도, 경도) 누락한 경우
+    console.log(`위도: ${lat} / 경도: ${lng}`);
+    return this.questService.getAll(lat, lng);
+  }
+
+  @Get(':quest_id')
+  getOne(@Param('quest_id') id: number): string {
+    console.log(`퀘스트id: ${id}`);
+    return this.questService.getOne(id);
+  }
+}
