ae7791d19689b118665480841bbd4f22d355f57f
diasm <diasm2@gmail.com>
2022-05-06 15:22:52 +0700

    [feat] 기본 swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger();
@@ -14,6 +15,17 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  // Swagger 설정
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('니땅내땅 API')
+    .setDescription('니땅내땅 API 테스트')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('/', app, document);
+
   const port = serverConfig.port;
   await app.listen(port);
   logger.log(`Application running on port ${port}`);
