e5943fcdd992df878bc20e7b921073d254d9670d
reload1bronze <nature9th@gmail.com>
2022-05-06 13:57:51 +0900

    [fix] 퀘스트 조회 요청시 좌표(위도, 경도) 누락 예외처리 -

--- src/quests/quests.controller.ts
+++ src/quests/quests.controller.ts
@@ -1,23 +1,36 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { QuestsService } from './quests.service';
 
-@Controller('quests')
+@Controller('/api/quests')
 export class QuestsController {
   constructor(private readonly questService: QuestsService) {}
 
+  /* 퀘스트 전체 조회 API */
   @Get()
   getAll(@Query('lat') lat: number, @Query('lng') lng: number) {
-    // TODO: (exception) 쿼리 파라미터(위도, 경도) 누락한 경우
-    console.log(`[컨트롤러] 위도: ${lat} / 경도: ${lng}`);
+    /* [예외처리] 쿼리 파라미터 누락: 위도(lat), 경도(lng) */
+    if (!lat || !lng) {
+      // return '위도(lat), 경도(lng)를 쿼리 파라미터로 보내주세요/';
+      throw new HttpException(
+        '위도(lat), 경도(lng)를 쿼리 파라미터로 보내주세요/',
+        HttpStatus.BAD_REQUEST
+      );
+    }
     return this.questService.getAll(lat, lng);
   }
 
+  /* 특정 퀘스트 조회 API */
   @Get(':quest_id')
   getOne(@Param('quest_id') id: number): string {
-    console.log(`[컨트롤러] 퀘스트id: ${id}`);
     return this.questService.getOne(id);
   }
-  
-  /* 퀘스트 수행 */
 
+  /* 퀘스트 수행 */
 }
