ea10e8b49647605ce7882136c7f2a95a3f5ccf1c
reload1bronze <nature9th@gmail.com>
2022-05-06 18:00:44 +0900

    [feat] 퀘스트 좌표 생성 알고리즘 - 처리 순서 1) 클라이언트로부터 플레이어의 좌표 받기 2) 좌표에 해당하는 행정동 확인 (kakao API 활용) 3) 행정동 내의 전체 주소 중 랜덤으로 특정 개수의 주소 선택 (행정안전부 도로명주소 개발자센터 활용) 4) 선택된 주소를 가지고 좌표 확인 (kakao API 활용)

--- src/quests/quests.service.ts
+++ src/quests/quests.service.ts
@@ -1,21 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { Observable } from 'rxjs';
-import { AxiosResponse } from 'axios';
-
-interface Address {
-  meta: {
-    total_count: number;
-  };
-  documents: [
-    {
-      read_address?: string;
-      address: {
-        [key: string]: string;
-      };
-    }
-  ];
-}
+import { map } from 'rxjs';
 
 @Injectable()
 export class QuestsService {
@@ -23,23 +8,74 @@ export class QuestsService {
 
   /* 위도(lat), 경도(lng) 기준으로 우리 마을 퀘스트 조회 */
   getAll(lat: number, lng: number) {
-    /* 카카오 API로 주소 받아오기 */
-    const address: Observable<AxiosResponse<Address>> = this.httpService.get(
-      `${process.env.KAKAO_BASE_URL}/v2/local/geo/coord2address.json?x=${lng}&y=${lat}&input_coord=WGS84`,
-      {
-        headers: {
-          Authorization: `KakaoAK ${process.env.REST_API_KEY}`,
-        },
-      }
-    );
+    /!* 카카오 API로 주소 받아오기 *!/;
+    const address = this.httpService
+      .get(
+        `${process.env.KAKAO_BASE_URL}/v2/local/geo/coord2address.json?x=${lng}&y=${lat}&input_coord=WGS84`,
+        {
+          headers: {
+            Authorization: `KakaoAK ${process.env.REST_API_KEY}`,
+          },
+        }
+      )
+      .pipe(map((res) => res.data.documents[0].address));
+    console.log(address);
 
-    address.subscribe((address) => {
-      console.log(address.data.documents[0].address); // 주소 반환
-      /* 받아온 주소 기준으로 랜덤 좌표 생성하기 */
-      // TODO: 랜덤 좌표 생성 기능 구현
+    /* 받아온 주소(동) 기준으로 랜덤 좌표 생성하기 */
+    address.subscribe((res) => {
+      console.log(res);
+      const dong = res.region_3depth_name;
+      console.log(dong);
 
-      return address.data.documents[0].address.region_3depth_name;
+      const addressByDong = this.httpService
+        .get(
+          `https://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=1&countPerPage=100&keyword=${encodeURI(
+            dong
+          )}&confmKey=${process.env.JUSO_CONFIRM_KEY}&resultType=json`
+        )
+        .pipe(map((res) => res.data));
+
+      addressByDong.subscribe((res) => {
+        const totalCount = res.results.common.totalCount;
+        // const lastPage = Math.ceil(totalCount / 100);
+        const lastPage = 3;
+        for (let i = 1; i <= lastPage; i++) {
+          const idx =
+            i !== lastPage
+              ? Math.floor(Math.random() * 100) + 1
+              : Math.floor(Math.random() * (totalCount % 100)) + 1;
+          console.log(`${i}번째 인덱스는 ${idx}`);
+          const result = this.httpService
+            .get(
+              `https://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=${i}&countPerPage=100&keyword=${encodeURI(
+                dong
+              )}&confmKey=${process.env.JUSO_CONFIRM_KEY}&resultType=json`
+            )
+            .pipe(map((res) => res.data.results.juso[idx].roadAddr));
+
+          result.subscribe((res) => {
+            const coords = this.httpService
+              .get(
+                `${
+                  process.env.KAKAO_BASE_URL
+                }/v2/local/search/address.json?query=${encodeURI(res)}`,
+                {
+                  headers: {
+                    Authorization: `KakaoAK ${process.env.REST_API_KEY}`,
+                  },
+                }
+              )
+              .pipe(map((res) => res.data));
+
+            coords.subscribe((res) => {
+              console.log(`좌표: ${res.documents[0].y}, ${res.documents[0].x}`);
+            });
+          });
+        }
+      });
     });
+
+    return address;
   }
 
   getOne(id: number): string {
