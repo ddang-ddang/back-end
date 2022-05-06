b5d94b28907d639bbb9bffb39d9342ed3abf6744
reload1bronze <nature9th@gmail.com>
2022-05-06 20:40:50 +0900

    [edit] 퀘스트 좌표 생성 매커니즘 주석 - 5/7(토), 5/9(월) 예정사항

--- src/quests/quests.service.ts
+++ src/quests/quests.service.ts
@@ -8,7 +8,22 @@ export class QuestsService {
 
   /* 위도(lat), 경도(lng) 기준으로 우리 마을 퀘스트 조회 */
   getAll(lat: number, lng: number) {
-    /!* 카카오 API로 주소 받아오기 *!/;
+    /* TODO: 공통
+     *       1. 좌표 기준으로 동 조회 (kakao API)
+     *       2. 오늘 날짜, 주소(시, 구, 동) 기준으로 DB 조회 (MySQL)
+     */
+    /* TODO: DB에 동 데이터 있는 경우
+     *       1. 퀘스트 조회, 완료여부 JOIN (MySQL)
+     *       2. 조회한 퀘스트들을 클라이언트로 보내줌
+     */
+    /* TODO: DB에 동 데이터가 없는 경우
+     *       1. 오늘 날짜, 주소(시, 구, 동) 포함한 동 데이터 DB에 추가 (MySQL)
+     *       2. 주소(동) 기준으로 랜덤 좌표 생성 (주소센터 API)
+     *       3. 랜덤 좌표마다 퀘스트 만들어서 DB에 추가 (MySQL)
+     *       4. 생성한 퀘스트들을 클라이언트로 보내줌
+     */
+
+    /* 좌표 기준으로 주소 조회 (kakao API) */
     const address = this.httpService
       .get(
         `${process.env.KAKAO_BASE_URL}/v2/local/geo/coord2address.json?x=${lng}&y=${lat}&input_coord=WGS84`,
@@ -19,33 +34,45 @@ export class QuestsService {
         }
       )
       .pipe(map((res) => res.data.documents[0].address));
-    console.log(address);
 
-    /* 받아온 주소(동) 기준으로 랜덤 좌표 생성하기 */
+    /* 오늘 날짜, 주소(시, 구, 동) 기준으로 DB 조회 (MySQL) */
+    // TODO: ~5/7(토) 구현 예정
+
+    // DB에 동 데이터 있는 경우
+    /* 퀘스트 조회, 완료여부 JOIN (MySQL) */
+    // TODO: ~5/9(월) 구현 예정
+    /* 조회한 퀘스트들을 클라이언트로 보내줌 */
+    // TODO: ~5/9(월) 구현 예정
+
+    // DB에 동 데이터가 없는 경우
+    /* 주소(동)에 해당하는 정보 조회 (주소센터 API) */
     address.subscribe((res) => {
-      console.log(res);
+      /* 오늘 날짜, 주소(시, 구, 동) 포함한 동 데이터 DB에 추가 (MySQL) */
+      // TODO: ~5/7(토) 구현 예정
       const dong = res.region_3depth_name;
-      console.log(dong);
-
-      const addressByDong = this.httpService
+      const allAddresses = this.httpService
         .get(
-          `https://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=1&countPerPage=100&keyword=${encodeURI(
+          `https://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=1&countPerPage=10&keyword=${encodeURI(
             dong
           )}&confmKey=${process.env.JUSO_CONFIRM_KEY}&resultType=json`
         )
         .pipe(map((res) => res.data));
 
-      addressByDong.subscribe((res) => {
+      /* 전체 주소를 돌면서 랜덤한 주소 추출 */
+      allAddresses.subscribe((res) => {
         const totalCount = res.results.common.totalCount;
-        // const lastPage = Math.ceil(totalCount / 100);
+        // TODO: 테스트용 - lastPage 3으로 제한
         const lastPage = 3;
+        // const lastPage = Math.ceil(totalCount / 100);
+        const questsArray = [];
         for (let i = 1; i <= lastPage; i++) {
           const idx =
             i !== lastPage
               ? Math.floor(Math.random() * 100) + 1
               : Math.floor(Math.random() * (totalCount % 100)) + 1;
           console.log(`${i}번째 인덱스는 ${idx}`);
-          const result = this.httpService
+
+          const randomAddress = this.httpService
             .get(
               `https://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=${i}&countPerPage=100&keyword=${encodeURI(
                 dong
@@ -53,8 +80,9 @@ export class QuestsService {
             )
             .pipe(map((res) => res.data.results.juso[idx].roadAddr));
 
-          result.subscribe((res) => {
-            const coords = this.httpService
+          /* 랜덤한 주소(동) 기준으로 좌표 생성하기 */
+          randomAddress.subscribe((res) => {
+            const questCoords = this.httpService
               .get(
                 `${
                   process.env.KAKAO_BASE_URL
@@ -67,11 +95,27 @@ export class QuestsService {
               )
               .pipe(map((res) => res.data));
 
-            coords.subscribe((res) => {
+            /* k. 랜덤 좌표마다 퀘스트 만들어서 DB에 추가 (MySQL) */
+            /* k-1. 랜덤 좌표마다 퀘스트를 배열로 저장 */
+            // TODO: ~5/7(토) 구현 예정
+            questCoords.subscribe((res) => {
               console.log(`좌표: ${res.documents[0].y}, ${res.documents[0].x}`);
+              const type = Math.floor(Math.random() * 3);
+              questsArray.push({
+                lat: res.documents[0].y,
+                lng: res.documents[0].x,
+                type,
+              });
+              console.log(questsArray);
             });
           });
         }
+        console.log(questsArray); // 빈 배열 반환
+        /* k-2. for문 다 돌고나서 DB에 추가 */
+        // TODO: ~5/7(토) 구현 예정 (observable subscribe가 핵심)
+
+        /* 생성한 퀘스트들을 클라이언트로 보내줌 */
+        // TODO: ~5/9(월) 구현 예정
       });
     });
 
