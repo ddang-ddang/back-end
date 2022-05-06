84c077bda30ca4e6fbf8874e8f8778ae5bc34c10
diasm <diasm2@gmail.com>
2022-05-06 22:19:45 +0700

    [fix] delete unused files

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from 'src/auth/auth.service';
