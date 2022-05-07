7b78472705b519bbea3d1273b7960efbaded0b2e
diasm <diasm2@gmail.com>
2022-05-07 17:43:56 +0700

    [fix] Repository 분류로 추가로 moudle 인젝트함

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
-import { JwtModule } from '@nestjs/jwt'
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { FeedsModule } from './feeds/feeds.module';
 import { CommentsModule } from './comments/comments.module';
 import { LikesModule } from './likes/likes.module';
 import { QuestsModule } from './quests/quests.module';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from '../ormconfig';
 import { AchievementsService } from './achievements/achievements.service';
@@ -15,13 +13,13 @@ import { AchievementsController } from './achievements/achievements.controller';
 import * as config from 'config';
 import { AuthModule } from './auth/auth.module';
 import { PlayersModule } from './players/players.module';
-import { Players } from './players/entities/player.entity';
 import { FeedRepository } from './feeds/feeds.repository';
 import { PassportModule } from '@nestjs/passport';
 import { PlayersController } from './players/players.controller';
 import { PlayersService } from './players/players.service';
 import { AuthService } from './auth/auth.service';
 import { FeedsService } from './feeds/feeds.service';
+import { PlayerRepository } from './players/players.repository';
 
 const jwtConfig = config.get('jwt');
 
@@ -33,11 +31,10 @@ const jwtConfig = config.get('jwt');
       signOptions: { expiresIn: '60s' },
     }),
     TypeOrmModule.forRoot(typeORMConfig),
-    TypeOrmModule.forFeature([Players, FeedRepository]),
+    TypeOrmModule.forFeature([PlayerRepository, FeedRepository]),
     AuthModule,
     PlayersModule,
     TypeOrmModule.forRoot(typeORMConfig),
-    UsersModule,
     FeedsModule,
     CommentsModule,
     LikesModule,
