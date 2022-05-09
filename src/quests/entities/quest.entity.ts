c475bb032bdcf6818ce57d9facfb2f0955218bd8
reload1bronze <nature9th@gmail.com>
2022-05-09 15:00:55 +0900

    [feat] TypeORM entity 설정 - module 파일에 entity, repository를 imports - 퀘스트, 동, 피드 entity 작성 및 수정

--- src/quests/entities/quest.entity.ts
+++ src/quests/entities/quest.entity.ts
@@ -1,1 +1,31 @@
-export class Quest {}
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Feed } from '../../feeds/entities/feed.entity';
+import { Dong } from './dong.entity';
+
+@Entity()
+export class Quest extends BaseEntity {
+  @PrimaryGeneratedColumn({ name: 'dongId' })
+  id: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  type: number;
+
+  @OneToMany((type) => Feed, (feed) => feed.quest)
+  feeds: Feed[];
+
+  @ManyToOne((type) => Dong, (dong) => dong.quests)
+  dong: Dong;
+}
--- src/quests/quests.module.ts
+++ src/quests/quests.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentRepository } from 'src/comments/comments.repository';
 import { FeedRepository } from 'src/feeds/feeds.repository';
+import { QuestsRepository } from 'src/quests/quests.repository';
 import { QuestsController } from './quests.controller';
 import { QuestsService } from './quests.service';
-import { HttpModule } from '@nestjs/axios';
+import { Quest } from './entities/quest.entity';
+import { Dong } from './entities/dong.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([Quest, Dong]),
+    TypeOrmModule.forFeature([QuestsRepository]),
     TypeOrmModule.forFeature([FeedRepository]),
     TypeOrmModule.forFeature([CommentRepository]),
-    HttpModule,
   ],
   controllers: [QuestsController],
   providers: [QuestsService],
