33e3f464e5d46e80cb02f95e3b68ab645305b7cb
reload1bronze <nature9th@gmail.com>
2022-05-10 16:50:19 +0900

    [fix] Complete 엔티티 프라이머리 키 누락 - 프라이머리키 추가

--- src/quests/entities/complete.entity.ts
+++ src/quests/entities/complete.entity.ts
@@ -1,9 +1,18 @@
-import { BaseEntity, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import {
+  BaseEntity,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Quest } from './quest.entity';
 import { Player } from '../../players/entities/player.entity';
 
 @Entity()
 export class Complete extends BaseEntity {
+  @PrimaryGeneratedColumn({ name: 'completeId' })
+  id: number;
+
   @ManyToOne(() => Player, (player) => player.completes)
   @JoinColumn()
   player: Player;
