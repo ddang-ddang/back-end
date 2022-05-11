7a87b713ae08945fc0a2d9e200875d693bff78e3
moto4321 <moto432195@gmail.com>
2022-05-11 20:18:07 +0900

    [fix] mock data 에러 수정

--- src/comments/test/stubs/comment.stub.ts
+++ src/comments/test/stubs/comment.stub.ts
@@ -1,23 +1,23 @@
 import { Comment } from 'src/comments/entities/comment.entity';
 import { Feed } from 'src/feeds/entities/feed.entity';
 
-export const commentStub = (): Comment => {
-  return {
-    id: 1,
-    comment: 'jest comment',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    deletedAt: null,
-    feed: {
-      id: 1,
-      image1_url: 'img1',
-      image2_url: 'img2',
-      image3_url: 'img3',
-      content: 'test content',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      deletedAt: new Date(),
-    },
-    player: {},
-  };
-};
+// export const commentStub = (): Comment => {
+  // return {
+  //   id: 1,
+  //   comment: 'jest comment',
+  //   createdAt: new Date(),
+  //   updatedAt: new Date(),
+  //   deletedAt: null,
+  //   feed: {
+  //     id: 1,
+  //     image1_url: 'img1',
+  //     image2_url: 'img2',
+  //     image3_url: 'img3',
+  //     content: 'test content',
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //     deletedAt: new Date(),
+  //   },
+  //   player: {},
+  // };
+// };
