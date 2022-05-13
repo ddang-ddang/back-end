e3f72159ed263c3f5a613f44278f53c8da21f27e
reload1bronze <nature9th@gmail.com>
2022-05-13 20:25:07 +0900

    [edit] 퀘스트 서비스 테스트코드 작성중 -

--- src/quests/quests.service.spec.ts
+++ src/quests/quests.service.spec.ts
@@ -1,18 +1,77 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { QuestsService } from './quests.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Feed } from 'src/feeds/entities/feed.entity';
+import { FeedRepository } from '../feeds/feeds.repository';
+import { CommentRepository } from '../comments/comments.repository';
+import { QuestsRepository } from './quests.repository';
+import { RegionsRepository } from './regions.repository';
+import { CompletesRepository } from './completes.repository';
+
+const mockRepository = {
+  createAndSave: jest.fn(),
+  findAll: jest.fn(),
+  findOneBy: jest.fn(),
+  findOneWithCompletes: jest.fn(),
+};
+
+const mockFeedRepository = {
+  feedQuest: jest.fn(),
+  updateFeed: jest.fn(),
+  deleteFeed: jest.fn(),
+};
+
+const mockCommentRepository = {};
+
+const mockQuestsRepository = {};
+
+const mockRegionsRepository = {};
+
+const mockCompletesRepository = {};
 
 describe('QuestsService', () => {
   let service: QuestsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [QuestsService],
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        // 퀘스트 서비스만 진짜, 나머지는 가짜(mock)
+        QuestsService,
+        // 레포지토리 필요 (독립적 테스팅)
+        {
+          provide: getRepositoryToken(Feed),
+          useValue: mockRepository,
+        },
+        {
+          provide: FeedRepository,
+          useValue: mockFeedRepository,
+        },
+        {
+          provide: CommentRepository,
+          useValue: mockCommentRepository,
+        },
+        {
+          provide: QuestsRepository,
+          useValue: mockQuestsRepository,
+        },
+        {
+          provide: RegionsRepository,
+          useValue: mockRegionsRepository,
+        },
+        {
+          provide: CompletesRepository,
+          useValue: mockCompletesRepository,
+        },
+      ],
     }).compile();
-
     service = module.get<QuestsService>(QuestsService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it.todo('questComplete');
+  it.todo('getAll');
+  it.todo('getOne');
 });
