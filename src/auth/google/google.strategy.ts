23e621e6e5a9054911039d95d3788b3682097a1b
diasm <diasm2@gmail.com>
2022-05-17 18:21:42 +0700

    [feat]카카오/구글 70%구현완료(가입인증, 로그인연결해야함)

--- src/auth/google/google.strategy.ts
+++ src/auth/google/google.strategy.ts
@@ -1,44 +1,37 @@
-import { CreateLikeDto } from './../../likes/dto/create-like.dto';
-// import { AuthService } from '../auth.service';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Strategy, Profile, VerifyCallback } from 'passport-google-oauth20';
 import * as config from 'config';
 
 const googleConfig = config.get('google');
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy) {
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
       clientID: googleConfig.clientId,
       clientSecret: googleConfig.clientSecret,
-      callbackURL: '/players/redirect',
+      callbackURL: '/api/players/googleauth',
       passReqToCallback: true,
       scope: ['profile', 'email'],
     });
   }
   // 인증이 되어 있나 감시
   async validate(
-    accessToken: string,
-    refreshToken: string,
+    _accessToken: string,
+    _refreshToken: string,
     profile: any,
-    Done: VerifyCallback
+    req: any
   ): Promise<any> {
-    console.log(googleConfig.clientId);
-    console.log(googleConfig.clientSecret);
-    // console.log(accessToken);
-    try {
-      console.log(profile);
-      const { emails, photos } = profile;
-      const player = {
-        email: emails[0].value,
-        profileImg: photos[0].value,
-        accessToken,
-      };
-      Done(null, player);
-    } catch (err) {
-      console.log(err);
-    }
+    const { id, emails, disaplayName } = req;
+    console.log(req);
+
+    console.log(profile);
+    return {
+      provider: 'google',
+      providerId: id,
+      nickname: disaplayName,
+      email: emails[0].value,
+    };
   }
 }
--- src/auth/kakao/kakao-strategy.ts
+++ src/auth/kakao/kakao-strategy.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-kakao';
 import * as config from 'config';
+import { AuthService } from '../auth.service';
 
 const kakaoConfig = config.get('kakao');
 
 @Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly authService: AuthService) {
     super({
       clientID: kakaoConfig.clientId,
       clientSecret: kakaoConfig.clientSecret,
-      callbackURL: '/players/kakaoredirect',
+      callbackURL: '/api/players/kakaoauth',
       //   scope: ['profile'],
     });
   }
@@ -25,13 +26,34 @@ export class KakaoStrategy extends PassportStrategy(Strategy) {
     const { id, username } = profile;
     const { profile_image, thumbnail_image } = profile._json.properties;
 
+    console.log(profile);
+    // DB조회 후 없으면 DB저장
+    // 저장될  provier id, provider, provider_id, email, nickname, profile_image, thumbnail_image
+    // if (id) {
+    // const hello = await this.authService.kakaoLogin(
+    //   email,
+    //   username,
+    //   profile_image,
+    //   'kakao',
+    //   id
+    // );
+
+    //   return hello;
+    // }
+
+    // const player = await this.authService.findOrCreatePlayer(
+
     const player = {
-      id: id,
-      username: username,
+      id,
+      username,
       profileImg: profile_image,
       thumbnailImg: thumbnail_image,
       accessToken: access_token,
+      refreshToken: refreshToken,
     };
+
+    console.log(player);
     Done(null, player);
+    return player;
   }
 }
