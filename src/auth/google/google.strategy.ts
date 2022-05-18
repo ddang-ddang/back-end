08cd253791ada0dbacd96e1c602d384331e460a2
diasm <diasm2@gmail.com>
2022-05-18 18:28:29 +0700

    [fix]구글 인증관련 수정

--- src/auth/google/google.strategy.ts
+++ src/auth/google/google.strategy.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Injectable, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, Profile, VerifyCallback } from 'passport-google-oauth20';
+import { PlayersService } from '../../players/players.service';
+import { AuthService } from '../auth.service';
 import * as config from 'config';
+const jwtConfig = config.get('jwt');
 
 const googleConfig = config.get('google');
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
+  private logger = new Logger('google');
+  JwtService: any;
+  constructor(
+    private readonly playersService: PlayersService,
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService
+  ) {
     super({
       clientID: googleConfig.clientId,
       clientSecret: googleConfig.clientSecret,
@@ -18,20 +28,48 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   }
   // 인증이 되어 있나 감시
   async validate(
-    _accessToken: string,
-    _refreshToken: string,
-    profile: any,
-    req: any
+    accessToken: string,
+    refreshToken: string,
+    user: any,
+    req: any,
+    done: VerifyCallback
   ): Promise<any> {
-    const { id, emails, disaplayName } = req;
-    console.log(req);
+    const { id, emails, displayName, photos } = req;
+    const isJoin = await this.authService.checkSignUp(id, 'google');
 
-    console.log(profile);
-    return {
+    if (!isJoin) {
+      this.logger.verbose(`${displayName}님이 구글로 회원가입을 진행합니다.`);
+      //가입
+      const joinGame = await this.playersService.signup({
+        email: id + '@ddangddang.com',
+        password: id + displayName,
+        nickname: displayName,
+        mbti: null,
+        profileImg: photos[0].value,
+        provider: 'google',
+        providerId: id,
+        // providerId: Number(String(id).substring(0, 19)),
+        currentHashedRefreshToken: refreshToken,
+      });
+      console.log(joinGame);
+    }
+
+    const player = {
       provider: 'google',
       providerId: id,
-      nickname: disaplayName,
+      nickname: displayName,
+      profileImg: photos[0].value,
       email: emails[0].value,
+      accessToken: this.jwtService.sign(
+        { id, email: emails[0].value },
+        {
+          secret: jwtConfig.accessSecret,
+          expiresIn: `${jwtConfig.accessTokenExp}s`,
+        }
+      ),
+      refreshToken: refreshToken,
     };
+
+    done(null, player);
   }
 }
