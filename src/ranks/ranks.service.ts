e340a2d4ded73118d5266401df6cf68840d68763
reload1bronze <nature9th@gmail.com>
2022-05-20 13:34:46 +0900

    [perf] 랭킹 조회 API 개선 - 쿼리빌더 적용

--- src/ranks/ranks.service.ts
+++ src/ranks/ranks.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Region } from '../quests/entities/region.entity';
-import { Repository } from 'typeorm';
-import { QuestRepository } from '../quests/repositories/quest.repository';
+import { getRepository, Repository } from 'typeorm';
 import { Complete } from '../quests/entities/complete.entity';
 import { Player } from '../players/entities/player.entity';
+import { Quest } from '../quests/entities/quest.entity';
 
 @Injectable()
 export class RanksService {
@@ -14,8 +14,7 @@ export class RanksService {
     @InjectRepository(Complete)
     private readonly completes: Repository<Complete>,
     @InjectRepository(Player)
-    private readonly players: Repository<Player>,
-    private readonly quests: QuestRepository
+    private readonly players: Repository<Player>
   ) {}
 
   private readonly logger = new Logger(RanksService.name);
@@ -25,25 +24,23 @@ export class RanksService {
 
     try {
       const { regionSi, regionGu, regionDong } = currentRegion; // 현재 지역 정보
-      const regionForCount = await this.regions.findOne({
-        where: { regionSi, regionGu, regionDong },
-      });
-      const { totalCount } = regionForCount; // 현재 지역 전체 주소
-
       const regions = await this.regions.find({
         where: { regionSi, regionGu, regionDong },
       });
       if (regions.length === 0)
         return { ok: false, message: '현재 위치를 찾을 수 없습니다.' };
 
-      // 현재 지역과 일치하는 모든 날짜의 지역 조회 (완료 테이블, 완료한 플레이어 조인)
+      const { totalCount } = regions[0];
+      // 모든 날짜의 현재 지역 데이터 조회 (완료 테이블, 완료한 플레이어 조인)
       const quests = await Promise.all([
         ...regions.map(async (region) => {
-          return await this.quests.find({
-            where: { region },
-            // TODO: relations 내에서 필요한 정보만 받을 수 있게 수정
-            relations: ['completes', 'completes.player'],
-          });
+          // TODO: relations 내에서 필요한 정보만 받을 수 있게 수정 (완료 정보가 있는, 플레이어 닉네임, 프로필이미지)
+          return await getRepository(Quest)
+            .createQueryBuilder('quest')
+            .where('regionId = :id', { id: region.id })
+            .innerJoinAndSelect('quest.completes', 'complete')
+            .leftJoinAndSelect('complete.player', 'player')
+            .getMany();
         }),
       ]);
 
@@ -94,19 +91,18 @@ export class RanksService {
    * @param {array} players - 퀘스트 유형별 완료한 플레이어
    * @param {number} totalCount - 해당 지역 전체 주소
    * @param {number} difficulty - 퀘스트 난이도 (포인트 차등)
-   * @returns {array} - 랭킹 리스트
+   * @returns {array} - TOP10 랭킹 리스트
    */
   async rankFor(
     players: number[],
     totalCount: number,
     difficulty: number
   ): Promise<object[]> {
-    // 플레이어당 퀘스트 완료횟수
+    // 플레이어당 퀘스트 완료횟수 (ex. { '96': 2, '99': 1, '100': 4 })
     const countFor = {};
     players.forEach((player) => {
       countFor[player] = (countFor[player] || 0) + 1;
     });
-    console.log(countFor); // { '96': 2, '99': 1, '100': 4 }
 
     // 객체를 배열로 변경 (promise.all 및 정렬을 위해서)
     const arrayFromObject = [];
@@ -115,7 +111,7 @@ export class RanksService {
     }
 
     // 랭킹 정보(플레이어, 포인트 등) 생성
-    const ranks = [];
+    const ranksTop10 = [];
     await Promise.all([
       ...arrayFromObject.map(async (player) => {
         const res = await this.players.findOne({ where: { id: player[0] } });
@@ -126,15 +122,12 @@ export class RanksService {
           counts: player[1],
           points: `${player[1] * 100 * difficulty}P`,
         };
-        ranks.push(rankingInfo);
+        ranksTop10.push(rankingInfo);
       }),
     ]);
 
-    // 랭킹 정하기 (내림차순 정렬)
-    ranks.sort(function (a, b) {
-      return b.counts - a.counts;
-    });
-    const top10 = ranks.slice(0, 10);
-    return top10;
+    // 랭킹 정렬
+    ranksTop10.sort((a, b) => b.counts - a.counts);
+    return ranksTop10.slice(0, 10);
   }
 }
