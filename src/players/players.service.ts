dba7de3c1c163248c48020d864cfdd2ac91327a7
diasm <diasm2@gmail.com>
2022-05-20 18:33:22 +0700

    [fix] mypage 1차 업데이트 완료

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -9,6 +9,8 @@ import {
 import { Player } from './entities/player.entity';
 import { PlayerRepository } from './players.repository';
 import * as bcrypt from 'bcrypt';
+import { Complete } from "../quests/entities/complete.entity";
+import { Mission } from "./entities/mission.entity";
 
 @Injectable()
 export class PlayersService {
@@ -121,7 +123,65 @@ export class PlayersService {
   }
 
   async mypageInfo(playerId: number): Promise<object> {
-    const result = await this.playersRepository.mypageInfo(playerId);
-    return result;
+    try {
+      //플레이어즈의 닉네임, mbti를 가져온다
+      const profile = await Player.createQueryBuilder('player')
+        .select([
+          'player.id',
+          'player.email',
+          'player.nickname',
+          'player.mbti',
+          'player.profileImg',
+          'player.level',
+          'player.exp',
+        ])
+        .leftJoinAndSelect('player.completes', 'completes')
+        .leftJoinAndSelect('completes.quest', 'quest')
+        .where('player.playerId = :playerId', { playerId: playerId })
+        .getMany();
+
+      const countEachType = await Complete.createQueryBuilder('complete')
+        .select(['quest.type', 'count(quest.type) as cnt'])
+        .leftJoin('complete.quest', 'quest')
+        .where('complete.playerId = :playerId', { playerId: playerId})
+        .groupBy('quest.type')
+        .getRawMany();
+
+      const missionList = await Mission.find({
+        order: {
+          createdAt: 'DESC',
+        },
+      });
+      const achievedMission = [];
+      const notAchievedMission = [];
+
+      // feed, mob, time으로 구별해서 mission에 저장되어있는 setGoals을 비교해서 결과값이 true이면 Achievement를 생성한다.
+      countEachType.map(async (cntItems) => {
+        missionList.map(async (mission) => {
+          if (
+            cntItems.quest_type === mission.type &&
+            cntItems.cnt >= mission.setGoals
+          ) {
+            achievedMission.push(mission);
+          }
+        });
+      });
+
+      missionList.map((mission) => {
+        if (!achievedMission.includes(mission)) {
+          notAchievedMission.push(mission);
+        }
+      });
+
+      // console.log(profile, missionList, achievedMission, notAchievedMission);
+
+      return {
+        profile,
+        achievedMission,
+        notAchievedMission,
+      };
+    } catch (err) {
+      return err.message;
+    }
   }
 }
