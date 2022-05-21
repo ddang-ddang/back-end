cec2865af5ea1cabfa301bfa5dfd555fe61803fc
diasm <diasm2@gmail.com>
2022-05-21 21:19:28 +0700

    [fix]불필요하내용 삭제

--- src/auth/session/session.seralizer.ts
+++ src/auth/session/session.seralizer.ts
@@ -19,17 +19,7 @@ export class SessionSerializer extends PassportSerializer {
     payload: any,
     done: (err: Error, payload: string) => void
   ): Promise<any> {
-<<<<<<< HEAD
     await this.authService.checkById(payload);
-=======
-    console.log('-----------------------------------');
-    const result = await this.authService.checkById(payload);
-    console.log(result);
-    console.log(payload);
-    console.log('디ㅏ시리얼 라이즈 되는 부분');
-    console.log('-----------------------------------');
-    // console.log()
->>>>>>> 56e9f943902094f2c0b836f4f9e8628b803f7ead
     done(null, payload);
   }
 }
