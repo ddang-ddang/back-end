e9afdf3c6e633950e184846912b53b111a145935
moto4321 <moto432195@gmail.com>
2022-05-23 17:37:10 +0900

    [refactor] entity return하지 않도록 수정

--- src/feeds/feeds.service.ts
+++ src/feeds/feeds.service.ts
@@ -136,7 +136,6 @@ export class FeedsService {
     feedContent: string
   ) {
     const feed = await this.findOneFeed(feedId);
-    console.log('aaaa');
     const match = await this.matchPlayerFeed(playerId, feed);
     if (feed) {
       if (match) {
