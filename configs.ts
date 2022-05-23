612db1969f6e1c30137efbdc13c228421beb02dc
diasm <diasm2@gmail.com>
2022-05-23 20:59:22 +0700

    [delete]필요없는 파일 삭제

--- configs.ts
+++ configs.ts
@@ -1,4 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import * as config from 'config';
 const dbConfig = config.get('db');
 const mapConfig2 = config.get('map');
