2897e69b1f7b1547e8284816b5a8ace67e989338
reload1bronze <nature9th@gmail.com>
2022-05-24 16:43:47 +0900

    [chore] quests, ranks 컨트롤러 스웨거 보충 - main.ts 파일에서 스웨거 설정 추가 - quests, ranks 컨트롤러에서 스웨서 응답 내용 추가

--- src/ranks/ranks.controller.ts
+++ src/ranks/ranks.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { RanksService } from './ranks.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiNotFoundResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('/api/ranks')
 @ApiTags('랭킹 조회 API')
@@ -8,7 +8,11 @@ export class RanksController {
   constructor(private readonly ranksService: RanksService) {}
 
   @Get()
-  @ApiOperation({ summary: '우리 지역 랭킹 조회 API' })
+  @ApiOperation({
+    summary: '우리 지역 랭킹 조회 API',
+    description: '지역 데이터를 기반으로 개인별, 그룹별 랭킹을 조회합니다.',
+  })
+  @ApiNotFoundResponse({ description: '랭킹을 조회할 수 없음' })
   getAll(
     @Query('si') regionSi: string,
     @Query('gu') regionGu: string,
